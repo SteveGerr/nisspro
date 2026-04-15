6917b767caf68c436bed8b0077b6c8bad689dd0c
i.shunyaev <i.shunyaev@fact.digital>
2026-04-15 15:22:02 +0400

    replaced initMap to privacy.js

--- main.js
+++ main.js
@@ -124,67 +124,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-        // Инициализация Яндекс карты
-    if (typeof ymaps !== 'undefined') {
-        ymaps.ready(initMap);
-    }
 
-    function initMap() {
-        const DARK_MAP = 'custom#dark';
-        ymaps.layer.storage.add(DARK_MAP, function DarkLayer() {
-            return new ymaps.Layer(
-            'https://core-renderer-tiles.maps.yandex.net/tiles?l=map&theme=dark&%c&%l&scale={{ scale }}'
-            );
-        });
 
-        // 2. Регистрируем новый тип карты
-        ymaps.mapType.storage.add(DARK_MAP, new ymaps.MapType('Dark Map', [DARK_MAP]));
-        // Координаты автосервиса
-        const center = [53.199417, 50.152565];
 
-        // Создание карты
-        const map = new ymaps.Map('yandex-map', {
-            center: center,
-            zoom: 17,
-            controls: ['zoomControl', 'fullscreenControl'],
-            type: 'custom#dark'
-        });
 
-        // Создание метки
-        const placemark = new ymaps.Placemark(center, {
-            hintContent: 'NissPro - автосервис',
-            balloonContent: `
-                <div class="map-balloon">
-                    <strong>NissPro</strong><br>
-                    Профессиональный автосервис<br>
-                    пр. Карла Маркса, 30А<br>
-                    Телефон: +7 (927) 295-30-30
-                </div>
-                <div class="map-balloon-img">
-                    <img src="images/facade.jpg" alt="NissPro">
-                </div>
-            `
-        }, {
-            iconLayout: 'default#image',
-            // iconImageHref: 'https://cdn-icons-png.flaticon.com/512/447/447031.png',
-            iconImageHref: './images/nissPro-logo-svg.svg',
-            iconImageSize: [30, 40],
-            iconImageOffset: [-20, -60],
-
-        });
-
-        // Добавление метки на карту
-        map.geoObjects.add(placemark);
-
-        // Добавление кнопки "Построить маршрут"
-        const routeButton = document.createElement('a');
-        routeButton.href = `https://yandex.ru/maps/?rtext=~${center[0]},${center[1]}&rtp=auto`;
-        routeButton.target = '_blank';
-        routeButton.className = 'map-route-button';
-        routeButton.innerHTML = '<i class="fas fa-route"></i> Построить маршрут';
-
-        document.querySelector('.map-container').appendChild(routeButton);
-    }
 
         // ========== ПАРАЛЛАКС ЭФФЕКТ ==========
     const layer1 = document.getElementById('layer1');
